975226676b1915d0619a848161a304c1dbf5c092
Stewart Jackson <stewart.jackson@gmail.com>
2018-05-13 23:08:41 -0400

    Added serial parsing for on/off. Added new off state.

--- src/arduino_espresso.ino
+++ src/arduino_espresso.ino
@@ -27,20 +27,29 @@ const int analogMaxPin = 2;
 const int analogMinPin = 3;
 const int analogTankMinPin = 4;
 
+const byte numChars = 32;
+
+/* Serial parsing */
+char receivedChars[numChars];
+boolean newData = false;
+
 boolean boilerIsFull = false;
 boolean boilerIsEmpty = false;
 boolean tankIsEmpty = false;
 boolean buttonIsPressed = false;
 boolean heatIsOn = false;
+boolean machineIsOn = false; //this is the state bit for whether the machine is active
 
 int lastState = -1;
 int state = -1;
-int nextState = 0;
+int nextState = 4; //start 'off' until button
 
 int lastBrewButtonState = HIGH;
 int brewButtonState = HIGH;
 int lastStopButtonState = HIGH;
 int stopButtonState = HIGH;
+boolean brewFlag = false;
+boolean stopFlag = false;
 
 int previousFlowmeterVal = 0x0;
 int flowmeterVal = 0x0;
@@ -48,6 +57,7 @@ int flowmeterVal = 0x0;
 int flowmeterCount = 0x0;
 
 unsigned long brewButtonDownTime = 0l;
+unsigned long stopButtonDownTime = 0l;
 
 void setup()
 {
@@ -61,14 +71,22 @@ void setup()
   pinMode(stopBrewButtonPin, INPUT);
   digitalWrite(stopBrewButtonPin, HIGH);
 
-  pinMode(boilerSolenoidRelayPin, OUTPUT);
-  pinMode(groupSolenoidRelayPin, OUTPUT);
-  pinMode(pumpRelayPin, OUTPUT);
-  pinMode(heatRelayPin, OUTPUT);
+  // LEDs are active low for this machine
+  pinMode(ledSinglePin, INPUT);
+  digitalWrite(ledSinglePin, HIGH);
+  pinMode(ledDoublePin, OUTPUT);
+  digitalWrite(ledDoublePin, LOW);
+  // digitalWrite(ledDoublePin, LOW);
+  pinMode(ledStopPin, OUTPUT);
+  digitalWrite(ledStopPin, HIGH);
 
-  digitalWrite(pumpRelayPin, RELAY_LOW);
+  pinMode(boilerSolenoidRelayPin, OUTPUT);
   digitalWrite(boilerSolenoidRelayPin, RELAY_LOW);
+  pinMode(groupSolenoidRelayPin, OUTPUT);
   digitalWrite(groupSolenoidRelayPin, RELAY_LOW);
+  pinMode(pumpRelayPin, OUTPUT);
+  digitalWrite(pumpRelayPin, RELAY_LOW);
+  pinMode(heatRelayPin, OUTPUT);
   digitalWrite(heatRelayPin, RELAY_LOW);
 
   Serial.println("starting up...");
@@ -78,6 +96,8 @@ void loop() {
   // these are done first because they have the ability to effect state. I think buttons need their own state in the future
   parseBrewButton();
   parseStopButton();
+  parseSerial();
+  showNewData();
 
   lastState = state;
   state = nextState;
@@ -85,6 +105,10 @@ void loop() {
   boilerIsFull = (analogRead(analogMaxPin) < BOILER_MAX_THRESH);
   boilerIsEmpty = (analogRead(analogMinPin) >= BOILER_MIN_THRESH);
   tankIsEmpty = (analogRead(analogTankMinPin) >= TANK_THRESH);
+
+  if (lastState != state) {
+    printStateInfo();
+  }
 
   switch (state) {
     case 1:
@@ -96,34 +120,13 @@ void loop() {
     case 3:
       tankEmptyState();
       break;
+    case 4:
+      offState();
+      break;
     default:
       baseState();
       break;
   }
-
-  if (lastState == state) return;
-
-  Serial.println("*************************");
-  Serial.print("last state: ");
-  Serial.print(lastState);
-  Serial.print(" state: ");
-  Serial.print(state);
-  Serial.print(" next state: ");
-  Serial.println(nextState);
-  Serial.print("boiler is empty: ");
-  Serial.print(analogRead(analogMinPin));
-  Serial.println(boilerIsEmpty ? " true" : " false");
-  Serial.print("boiler is full: ");
-  Serial.print(analogRead(analogMaxPin));
-  Serial.println(boilerIsFull ? " true" : " false");
-  Serial.print("tank is empty: ");
-  Serial.print(analogRead(analogTankMinPin));
-  Serial.println(tankIsEmpty ? " true" : " false");
-  Serial.print("button is pressed: ");
-  Serial.println(buttonIsPressed ? "true" : "false");
-  Serial.print("heat is on: ");
-  Serial.println(heatIsOn ? "true" : "false");
-  Serial.println("*************************");
 }
 
 // state 0
@@ -135,9 +138,10 @@ void baseState() {
 
   // NOTE: tank min value should prevent a shot from being pulled, but shouldn't immediately kill a shot being poured
 
-  // tank has water and boiler is empty
-  if (!tankIsEmpty && boilerIsEmpty) {
-    nextState = 2;
+  if (!machineIsOn) {
+    nextState = 4;
+  } else if (!tankIsEmpty && boilerIsEmpty) {
+    nextState = 2; // tank has water and boiler is empty
   } else if (!tankIsEmpty && buttonIsPressed) {
     nextState = 1;
   } else if (tankIsEmpty) {
@@ -185,7 +189,7 @@ void pullAShotState() {
 
 // state 2
 void fillBoilerState() {
-  // tank is done filling
+  // exit condition: tank is done filling
   if (boilerIsFull) {
     nextState = 0;
     return;
@@ -208,7 +212,7 @@ void fillBoilerState() {
 
 // state 3
 void tankEmptyState() {
-//  Serial.println("state: tank is empty");
+  // exit condition: tank is no longer empty
   if (!tankIsEmpty) {
     nextState = 0;
     return;
@@ -221,6 +225,24 @@ void tankEmptyState() {
 
   nextState = 3;
 
+}
+
+// state 4
+void offState() {
+  // exit condition: machine is turned on
+  if (buttonIsPressed || machineIsOn) {
+    machineIsOn = true;
+    buttonIsPressed = false;
+    nextState = 0;
+    return;
+  }
+
+  digitalWrite(pumpRelayPin, RELAY_LOW);
+  digitalWrite(boilerSolenoidRelayPin, RELAY_LOW);
+  digitalWrite(groupSolenoidRelayPin, RELAY_LOW);
+  toggleHeat(false); // added protection for heater if tank and boiler are empty
+
+  nextState = 4;
 }
 
 void toggleHeat(boolean heat) {
@@ -238,14 +260,20 @@ void parseBrewButton() {
     brewButtonDownTime = millis();
   }
 
-  // this is a button toggle
-  else if (lastBrewButtonState == LOW && brewButtonState == HIGH) {
-    buttonIsPressed = true;
+  else if (machineIsOn && brewButtonState == LOW && brewButtonDownTime + 1000l < millis()) {
+    nextState = 2;
+    brewFlag = true;
   }
 
-  // way to force a fill tank by pushing two buttons for 3 seconds
-  else if (brewButtonState == LOW && stopButtonState == LOW && brewButtonDownTime + 3000l > millis()) {
-    nextState = 2;
+  // this is a button toggle
+  else if (lastBrewButtonState == LOW && brewButtonState == HIGH) {
+    if (!machineIsOn) {
+      machineIsOn = true;
+    } else if (brewFlag) {
+      brewFlag = false;
+    } else {
+      buttonIsPressed = true;
+    }
   }
 }
 
@@ -253,8 +281,112 @@ void parseStopButton() {
   lastStopButtonState = stopButtonState;
   stopButtonState = digitalRead(stopBrewButtonPin);
 
-  // this is a button toggle
-  if (lastStopButtonState == LOW && stopButtonState == HIGH) {
-    buttonIsPressed = false;
+  if (lastStopButtonState == HIGH && stopButtonState == LOW) {
+    stopButtonDownTime = millis();
+  } else if (stopButtonState == LOW && !buttonIsPressed && machineIsOn && (stopButtonDownTime + 1000l) < millis()) {
+      machineIsOn = false;
+      stopFlag = true;
   }
+
+  // this is a button toggle
+  else if (lastStopButtonState == LOW && stopButtonState == HIGH) {
+    if (stopFlag) {
+      stopFlag = false;
+      return;
+    }
+      buttonIsPressed = false;
+      machineIsOn = true;
+  }
+}
+
+void printStateInfo() {
+  Serial.println("*************************");
+  Serial.print("last state: ");
+  Serial.print(lastState);
+  Serial.print(" state: ");
+  Serial.print(state);
+  Serial.print(" next state: ");
+  Serial.println(nextState);
+  Serial.print("boiler is empty: ");
+  Serial.print(analogRead(analogMinPin));
+  Serial.println(boilerIsEmpty ? " true" : " false");
+  Serial.print("boiler is full: ");
+  Serial.print(analogRead(analogMaxPin));
+  Serial.println(boilerIsFull ? " true" : " false");
+  Serial.print("tank is empty: ");
+  Serial.print(analogRead(analogTankMinPin));
+  Serial.println(tankIsEmpty ? " true" : " false");
+  Serial.print("button is pressed: ");
+  Serial.println(buttonIsPressed ? "true" : "false");
+  Serial.print("heat is on: ");
+  Serial.println(heatIsOn ? "true" : "false");
+  Serial.print("machine is on: ");
+  Serial.println(machineIsOn ? "true" : "false");
+  Serial.println("*************************");
+}
+
+/* Serial parsing */
+
+void parseSerial() {
+    static boolean recvInProgress = false;
+    static byte ndx = 0;
+    char startMarker = '<';
+    char endMarker = '>';
+    char rc;
+
+    // strings max at 32 bits
+
+    while (Serial.available() > 0 && newData == false) {
+        rc = Serial.read();
+
+        if (recvInProgress == true) {
+            if (rc != endMarker) {
+                receivedChars[ndx] = rc;
+                ndx++;
+                if (ndx >= numChars) {
+                    ndx = numChars - 1;
+                }
+            }
+            else {
+                receivedChars[ndx] = '\0'; // terminate the string
+                recvInProgress = false;
+                ndx = 0;
+                newData = true;
+            }
+        }
+
+        else if (rc == startMarker) {
+            recvInProgress = true;
+        }
+    }
+}
+
+void showNewData() {
+    if (newData == true) {
+      newData = false;
+      String a = String(receivedChars);
+
+      if (a == "on") {
+        machineIsOn = true;
+      } else if (a == "off") {
+        machineIsOn = false;
+      } else if (a == "f") {
+        if (machineIsOn) {
+          nextState = 2;
+        } else {
+          Serial.println("<ERROR>");
+          return;
+        }
+      } else if (a == "s") {
+        Serial.println(state);
+        return;
+      } else if (a == "v") {
+        printStateInfo();
+        return;
+      } else {
+        Serial.println("<UNKNOWN>");
+         return;
+      }
+      Serial.println("<OK>");
+    }
 }
